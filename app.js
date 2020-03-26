8a0015489f7e37c758859551b9c3fe74638a3152
abhinav-TB <abhinavtb@gmail.com>
2020-03-26 17:32:50 +0530

    home page made

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = process.env.PORT | 5000;
 app.set('view engine', 'ejs');
 app.use("/assets/css", express.static(__dirname + "/assets/css"));
+
 app.use("/assets/img", express.static(__dirname + "/assets/img"));
+app.use("/assets/img", express.static(__dirname + "/assets/scss"));
 app.use("/assets/js", express.static(__dirname + "/assets/js"));
 app.get('/', function(req, res) {
 	res.render('home');
